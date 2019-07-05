2a650c3ec4bd04e4dd062dc2b0a8bc61b6f23660
latesil <skipastas@gmail.com>
2019-07-06 00:46:38 +0300

    all works

--- theme-switcher-auto.sh
+++ theme-switcher-auto.sh
@@ -4,8 +4,8 @@ time=$(date +"%H")
 
 # List and set your gnome-terminal profiles:
 # gsettings get org.gnome.Terminal.ProfilesList list
-#terminal_dark='88173e30-df6e-4442-b012-4e1119c7385f'
-#terminal_light='b4bd0ffd-117e-4778-82ef-da4ccdf4cb2c'
+terminal_dark='88173e30-df6e-4442-b012-4e1119c7385f'
+terminal_light='b4bd0ffd-117e-4778-82ef-da4ccdf4cb2c'
 night=$(gsettings get org.theme-switcher nighttime)
 daytime=$(gsettings get org.theme-switcher daytime)
 night_wallpapers=$(gsettings get org.theme-switcher path-to-night-wallpaper)
@@ -16,13 +16,13 @@ if (( $time >= $night )) || (( $time =< $daytime )); then
     if [ "$night_wallpapers" != "''" ]; then
         gsettings set org.gnome.desktop.background picture-uri "$night_wallpapers"
     fi
-    #gsettings set org.gnome.Terminal.ProfilesList default $terminal_dark
+    gsettings set org.gnome.Terminal.ProfilesList default $terminal_dark
     exit 0
 else
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
     if [ "$day_wallpapers" != "''" ]; then
         gsettings set org.gnome.desktop.background picture-uri "$day_wallpapers"
     fi
-    #gsettings set org.gnome.Terminal.ProfilesList default $terminal_light
+    gsettings set org.gnome.Terminal.ProfilesList default $terminal_light
     exit 0
 fi
