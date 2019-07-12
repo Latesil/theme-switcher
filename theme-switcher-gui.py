f3c60ac0bccd8d778c0dda05e187610f58ae6571
latesil <skipastas@gmail.com>
2019-07-12 15:58:41 +0300

    fix theme show

--- theme-switcher-gui.py
+++ theme-switcher-gui.py
@@ -64,6 +64,7 @@ class Popover(Gtk.PopoverMenu):
     _change_theme_button = Gtk.Template.Child()
     _reset_all_button = Gtk.Template.Child()
     _reset_time_button = Gtk.Template.Child()
+    _reset_themes = Gtk.Template.Child()
     _reset_wallpapers = Gtk.Template.Child()
     _about_button = Gtk.Template.Child()
 
@@ -89,6 +90,11 @@ class Popover(Gtk.PopoverMenu):
         self.settings.set_int("daytime", 6)
 
     @Gtk.Template.Callback()
+    def on__reset_themes_clicked(self, button):
+        self.settings.set_string("light-theme", "Adwaita")
+        self.settings.set_string("dark-theme", "Adwaita-dark")
+
+    @Gtk.Template.Callback()
     def on__reset_wallpapers_clicked(self, button):
         self.settings.set_string("path-to-night-wallpaper", "")
         self.settings.set_string("path-to-day-wallpaper", "")
@@ -97,6 +103,7 @@ class Popover(Gtk.PopoverMenu):
     def on__reset_all_button_clicked(self, button):
         self.on__reset_time_button_clicked(button)
         self.on__reset_wallpapers_clicked(button)
+        self.on__reset_themes_button_clicked(button)
         self.settings.set_boolean("auto-switch", True)
 
     @Gtk.Template.Callback()
@@ -228,33 +235,29 @@ class MiddleGrid(Gtk.Grid):
         self.current_theme = self.theme_settings.get_string("gtk-theme")
 
         self.main_settings = Gio.Settings.new(BASE_KEY)
+
         self.current_light_theme = self.main_settings.get_string("light-theme")
         self.current_dark_theme = self.main_settings.get_string("dark-theme")
+
+        self.main_settings.connect("changed::light-theme", self.on_light_theme_change, self._light_combo_box)
+        self.main_settings.connect("changed::dark-theme", self.on_dark_theme_change, self._dark_combo_box)
 
         self._light_theme_label.set_halign(Gtk.Align.START)
         self._dark_theme_label.set_halign(Gtk.Align.START)
         self._light_combo_box.set_margin_end(10)
         self._light_combo_box.set_name("light_box")
         self._dark_combo_box.set_name("dark_box")
-        if self.current_theme not in themes:
-            self._light_tree_model.append([self.current_theme])
+        # if self.current_theme not in themes:
+        #     self._light_tree_model.append([self.current_theme])
 
         #populate theme list
         for i in themes:
             self._light_tree_model.append([i])
             self._dark_tree_model.append([i])
 
-        #retrieve current light theme and set it as a default option in combo box
-        self._light_model = self._light_combo_box.get_model()
-        for row in self._light_model:
-            if row[0] == self.current_light_theme:
-                self._light_combo_box.set_active_iter(row.iter)
-
-        #retrieve current dark theme and set it as a default option in combo box
-        self._dark_model = self._dark_combo_box.get_model()
-        for row in self._dark_model:
-            if row[0] == self.current_dark_theme:
-                self._dark_combo_box.set_active_iter(row.iter)
+        #retrieve current light\dark theme and set it as a default option in combo box
+        self.retrieve_light_theme(self._light_combo_box)
+        self.retrieve_dark_theme(self._dark_combo_box)
 
         #init light box
         self._light_combo_box.connect("changed", self.combo_box_changed)
@@ -267,6 +270,26 @@ class MiddleGrid(Gtk.Grid):
         renderer_text = Gtk.CellRendererText()
         self._dark_combo_box.pack_start(renderer_text, True)
         self._dark_combo_box.add_attribute(renderer_text, "text", 0)
+
+    def on_light_theme_change(self, settings, key, box):
+        self.current_light_theme = self.main_settings.get_string("light-theme")
+        self.retrieve_light_theme(box)
+
+    def on_dark_theme_change(self, settings, key, box):
+        self.current_dark_theme = self.main_settings.get_string("dark-theme")
+        self.retrieve_dark_theme(box)
+
+    def retrieve_light_theme(self, box):
+        self._light_model = box.get_model()
+        for row in self._light_model:
+            if row[0] == self.current_light_theme:
+                box.set_active_iter(row.iter)
+
+    def retrieve_dark_theme(self, box):
+        self._dark_model = box.get_model()
+        for row in self._dark_model:
+            if row[0] == self.current_dark_theme:
+                box.set_active_iter(row.iter)
 
 
     def combo_box_changed(self, combo):
