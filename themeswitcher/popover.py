1f4ba0e9a91d4715ba1ef14d09c82de38b9bcd46
Latesil <skipastas@gmail.com>
2020-03-17 14:14:23 +0300

    popover

--- themeswitcher/popover.py
+++ themeswitcher/popover.py
@@ -1,4 +1,5 @@
 from .theme_switcher_constants import theme_switcher_constants as constants
+from .helper_functions import init_de
 import locale
 import subprocess
 import os
@@ -8,6 +9,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 
+desktop = init_de()
 
 @Gtk.Template(resource_path = constants["UI_PATH"] + 'ui/popover.ui')
 class Popover(Gtk.PopoverMenu):
@@ -24,9 +26,6 @@ class Popover(Gtk.PopoverMenu):
     def __init__(self):
         super().__init__()
 
-        #init settings
-        self.settings = Gio.Settings.new(constants["BASE_KEY"])
-
     #callback for change theme now
     @Gtk.Template.Callback()
     def on__change_theme_button_clicked(self, button):
@@ -39,25 +38,25 @@ class Popover(Gtk.PopoverMenu):
 
     @Gtk.Template.Callback()
     def on__reset_time_button_clicked(self, button):
-        self.settings.set_int("nighttime", 20)
-        self.settings.set_int("daytime", 6)
+        desktop.set_value("nighttime", 20)
+        desktop.set_value("daytime", 6)
 
     @Gtk.Template.Callback()
     def on__reset_themes_clicked(self, button):
-        self.settings.set_string("light-theme", constants["default_light_theme"])
-        self.settings.set_string("dark-theme", constants["default_dark_theme"])
+        desktop.set_value("light-theme", constants["default_light_theme"])
+        desktop.set_value("dark-theme", constants["default_dark_theme"])
 
     @Gtk.Template.Callback()
     def on__reset_wallpapers_clicked(self, button):
-        self.settings.set_string("path-to-night-wallpaper", "")
-        self.settings.set_string("path-to-day-wallpaper", "")
+        desktop.set_value("path-to-night-wallpaper", "")
+        desktop.set_value("path-to-day-wallpaper", "")
 
     @Gtk.Template.Callback()
     def on__reset_all_button_clicked(self, button):
         self.on__reset_time_button_clicked(button)
         self.on__reset_wallpapers_clicked(button)
         self.on__reset_themes_clicked(button)
-        self.settings.set_boolean("auto-switch", True)
+        desktop.set_value("auto-switch", True)
 
     @Gtk.Template.Callback()
     def on__about_button_clicked(self, button):
